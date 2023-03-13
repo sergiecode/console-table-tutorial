17d7dabae64a9b9b73f5859c04c983358b95f82d
SnSDk <sergiosnsdk@gmail.com>
2023-03-13 13:08:21 -0300

    console-table-tutorial

---
+++ console-table.js
@@ -0,0 +1,16 @@
+
+
+const tecnologiasWeb = [
+    {nombre: 'Html', complejidad: 1, obligatorio: true},
+    {nombre: 'Css', complejidad: 2, obligatorio: true},
+    {nombre: 'Javascript', complejidad: 7, obligatorio: true},
+    {nombre: 'Typescript', complejidad: 9, obligatorio: false},
+    {nombre: 'ReactJs', complejidad: 7, obligatorio: false},
+    {nombre: 'Angular', complejidad: 1, obligatorio: false},
+]
+
+console.table(tecnologiasWeb)
+
+
+
+
